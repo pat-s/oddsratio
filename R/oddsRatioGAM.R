2bbacbead488bc241cacaf7b069c98d101499d07
Patrick Johann Schratz <patrick.schratz@gmail.com>
2016-09-05 19:31:51 +0200

    - changed function name - added output to console of OR calculation   -- added argument to disable console output (quietly) - stored returned information in DF columns

--- R/oddsRatioGAM.R
+++ R/oddsRatioGAM.R
@@ -1,12 +1,8 @@
-oddsratioGAM <- function(model, data, steps,
-                         predictors = NULL, pred, slice = FALSE,
-                         values) {
+calc_oddsratio.gam <- function(data, model, pred, values, 
+                          steps, slice = FALSE, quietly = FALSE) {
   
-  # get names of preds
-  if (!is.null(predictors)) {
-    names_pred <- predictors
-  }
-  else names_pred <- colnames(data)
+
+  names_pred <- colnames(data)
   
   if (slice) {
     
@@ -20,7 +16,18 @@ oddsratioGAM <- function(model, data, steps,
       range_v <- c(range_v, min(data[, pred]) + step * i)
     }
     
-    result <- data.frame(nrow = 1)
+    result <- data.frame(odds_ratio = numeric(length = steps),
+                         from = numeric(length = steps),
+                         to = numeric(length = steps),
+                         perc_from = character(length = steps),
+                         perc_to = character(length = steps),
+                         stringsAsFactors=FALSE)
+    
+    
+    if(!quietly)
+      # print variable information
+      cat("Predictor: '", pred, "'\nSteps: ", steps, " (", 100/steps, "%)\n", sep="")
+    
     # apply OR calc for vector
     for (x in 1:(steps)) {
       # set all preds to their mean
@@ -28,7 +35,7 @@ oddsratioGAM <- function(model, data, steps,
         data[[i]] <- mean(data[[i]])
       }
       # reduce to 1 row
-      data <- dplyr::slice(data, 1:1)
+      data <- data[1, ]
       # subset DF to preds only
       data <- data[, names_pred]
       
@@ -40,16 +47,24 @@ oddsratioGAM <- function(model, data, steps,
       data[, pred] <- range_v[x+1]
       # calc log odds for value 2
       pred_gam2 <- as.numeric(predict(model, data, type = "link"))
+      odds_ratio <- as.numeric(predict(model, data, type = "link"))
       
-      odds_ratio <- as.numeric(exp(pred_gam2 - pred_gam1), 2)
-      value_info <- list(paste0("OR (", round(range_v[x], 3), " to ", round(range_v[x+1], 3), ")"))
       
-      tmp <- data.frame(odds_ratio)
-      names(tmp) <- value_info
+      result$odds_ratio[x] <- as.numeric(exp(pred_gam2 - pred_gam1), 2)
+      result$from[x] <- round(range_v[x], 3)
+      result$to[x] <-  round(range_v[x+1], 3)
+      result$perc_from[x] <- as.character((100/steps*x - 100/steps))
+      result$perc_to[x] <- as.character((100/steps)*x)
+
       
-      result <- cbind(result, tmp)
+      if (!quietly) {
+        cat("\nOdds ratio from ", round(range_v[x], 3), "(", 
+            (100/steps*x - 100/steps), "%)", " to ", 
+            round(range_v[x+1], 3), 
+            "(", (100/steps)*x, "%): ", 
+            as.numeric(exp(pred_gam2 - pred_gam1), 2), sep = "")
+      }
     }
-    result[, 1] <- NULL
     return(result)
   }
   
@@ -59,7 +74,7 @@ oddsratioGAM <- function(model, data, steps,
   }
   
   # reduce to 1 row
-  data <- dplyr::slice(data, 1:1)
+  data <- data[1, ]
   # subset DF to preds only
   data <- data[, names_pred]
   
@@ -75,6 +90,10 @@ oddsratioGAM <- function(model, data, steps,
   
   odds_ratio <- as.numeric(exp(pred_gam2 - pred_gam1), 2)
   value_info <- list(paste0("OR (", values[1], " to ", values[2], ")"))
+  
+  if (!quietly & !slice)
+    # print variable information
+    cat("Predictor: '", pred, "'\n", sep = "")
   
   # return named array
   array(odds_ratio, dimnames = value_info)
