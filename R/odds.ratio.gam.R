19396f523971988d96a91c876e6f3f768c454863
pat-s <patrick.schratz@gmail.com>
2016-10-11 15:40:41 +0200

    update .gam example

--- R/odds.ratio.gam.R
+++ R/odds.ratio.gam.R
@@ -48,7 +48,7 @@
 #' library(mgcv)
 #' n <- 200
 #' sig <- 2
-#' dat <- gamSim(1, n = n,scale = sig)
+#' dat <- gamSim(1, n = n,scale = sig, verbose = FALSE)
 #' dat$x4 <- as.factor(c(rep("A", 50), rep("B", 50), rep("C", 50), rep("D", 50)))
 #' fit.gam <- gam(y ~ s(x0) + s(I(x1^2)) + s(x2) + 
 #'                offset(x3) + x4, data = dat) # fit model
