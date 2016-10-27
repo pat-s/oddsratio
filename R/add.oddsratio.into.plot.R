1bb69f5b5288b4e22c02b1811d3177cb8b307154
pat-s <patrick.schratz@gmail.com>
2016-10-27 22:05:24 +0200

    prepare for CRAN release

--- R/add.oddsratio.into.plot.R
+++ R/add.oddsratio.into.plot.R
@@ -104,16 +104,14 @@ add.oddsratio.into.plot <- function(
     if (is.null(rect.col)) {
       rect.col = text.col
     }
-    # backup data
-    #plot.object1 <- plot.object
-    
-    # set drawing order to place rect behind smoothing fun
+
+    #set drawing order to place rect behind smoothing fun
     plot.object$layers <- c(geom_rect(data = plot.object$data[1,], # avoids multiple rect drawings
-                                      ymin = ggplot_build(plot.object)$layout$panel_ranges[[1]]$y.range[1], 
-                                      ymax = ggplot_build(plot.object)$layout$panel_ranges[[1]]$y.range[2],
+                                      ymin = ggplot_build(plot.object)$panel$ranges[[1]]$y.range[1],
+                                      ymax = ggplot_build(plot.object)$panel$ranges[[1]]$y.range[2],
                                       xmin = or.object$value1,
-                                      xmax = or.object$value2, 
-                                      alpha = rect.alpha, fill = rect.col), 
+                                      xmax = or.object$value2,
+                                      alpha = rect.alpha, fill = rect.col),
                             plot.object$layers)
   }
 
