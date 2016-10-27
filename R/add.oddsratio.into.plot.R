f2ed23f9b1eaf250d3d4fe4c9336c38294b2fe71
pat-s <patrick.schratz@gmail.com>
2016-10-27 13:14:06 +0200

    update arrow feature in add.oddsratio.into.plot

--- R/add.oddsratio.into.plot.R
+++ R/add.oddsratio.into.plot.R
@@ -9,21 +9,21 @@
 #' 
 #' @param plot.object A `ggplot` object from \code{\link[oddsratio]{plot_smooth.gam}}
 #' @param or.object A returned data.frame from \code{\link[oddsratio]{calc.oddsratio.gam}}
-#' @param col.line Character. Color of the vertical line showing the predictor values
-#' @param col.text Character. Color of the inserted odds ratio information
 #' @param values Logical. Whether to print predictor value information nearby 
 #' the inserted vertical lines. Default to \code{TRUE}.
-#' @param height.or Numeric. Specifies y-location of inserted odds ratio information.
+#' @param rect Logical. Whether to print a shaded rectangle between the vertical lines.
+#' @param arrow Logical. Wheter to print arrows above the inserted values. Default to \code{TRUE}.
+#' @param line.col,line.alpha,line.type,line.size Aesthetics of vertical lines. 
+#' @param text.col,text.alpha,text.size Aesthetics of inserted values .
+#' @param or.yloc,values.yloc Numeric. Specifies y-location of inserted odds ratio / values.
 #' Relative to plotted y-axis range. A positive/negative value will place the 
-#' odds ratio information higher/lower.  
-#' @param height.val Numeric. Specifies y-location of inserted predictor information.
-#' Relative to plotted y-axis range. A positive/negative value will place the 
-#' predictor information higher/lower.  
-#' @param x.shift Numeric. Placement of predictor information relative to its vertical line.
-#' Relative to x-axis range. Default to \code{0.02}. 
-#' Play around to find the perfect position for you.
+#' the text higher/lower.  
+#' @param values.xloc Numeric. X-axis location/shift of values relative to their vertical line.
+#' Default to 2\% of x-axis range. 
+#' @param arrow.xloc.r,arrow.xloc.l,arrow.yloc,arrow.length,arrow.col Numeric. Axis placement options of inserted arrows.
+#' Relative to respective axis ranges.
 #' 
-#' @details The logic behind this function is to add calculated odds ratio of 
+#' @details The idea behind this function is to add calculated odds ratio of 
 #' fitted GAM models (\code{\link[oddsratio]{calc.oddsratio.gam}}) into a plot 
 #' showing the smooth function (\code{\link[oddsratio]{plot_smooth.gam}}) of the chosen 
 #' predictor for which the odds ratio was calculated for. Multiple insertions can 
@@ -55,36 +55,37 @@
 #' library(oddsratio)            
 #' plot.object <- pl.smooth.gam(fit.gam, pred = "x2", title = "Predictor 'x2'")
 #' or.object1 <- calc.oddsratio.gam(data = dat, model = fit.gam, pred = "x2", 
-#'                                values = c(0.099, 0.198))
+#'                                  values = c(0.099, 0.198))
 #'                                
 #' # insert first odds ratios to plot
-#' plot.object <- add.oddsratio.into.plot(plot.object, or.object1, or.height = 3,
-#'                                        x.shift = 0.04, line.size = 0.5, 
+#' plot.object <- add.oddsratio.into.plot(plot.object, or.object1, or.yloc = 3,
+#'                                        values.xloc = 0.04, line.size = 0.5, 
 #'                                        line.type = "dotdash", text.size = 6,
-#'                                        values.height = 0.5)
+#'                                        values.yloc = 0.5, arrow.col = "red")
 #'
 #' # calculate second odds ratio
 #' or.object2 <- calc.oddsratio.gam(data = dat, model = fit.gam, pred = "x2", 
 #'                                  values = c(0.4, 0.6))
 #'                                   
 #' # add or.object2 into plot                                  
-#' add.oddsratio.into.plot(plot.object, or.object2, or.height = 0, 
-#'                         line.col = "green4", text.col = "green4", 
-#'                         line.alpha = 0.5, line.type = "dashed",
+#' add.oddsratio.into.plot(plot.object, or.object2, or.yloc = 0.1, 
+#'                         line.col = "green4", text.col = "black",
+#'                         rect.col = "green4", rect.alpha = 0.2,
+#'                         line.alpha = 1, line.type = "dashed",
 #'                         arrow.xloc.r = 0.01, arrow.xloc.l = -0.01,
-#'                         arrow.length = 0.01)          
+#'                         arrow.length = 0.01, rect = T)          
 #' @export                       
 
-add.oddsratio.into.plot <- function(plot.object, or.object, line.col = "red",
-                                    line.size = 1.2, line.type = "solid", 
-                                    line.alpha = 1, text.alpha = 1, 
-                                    text.size = 4, arrow.length = NULL, 
-                                    arrow.height = NULL,
-                                    text.col = "red", values = TRUE,
-                                    or.height = 0, values.height = 0,
-                                    x.shift = NULL, arrow = TRUE,
-                                    arrow.xloc.r = NULL, arrow.xloc.l = NULL) 
-  {
+add.oddsratio.into.plot <- function(
+  plot.object, or.object, 
+  line.col = "red", line.size = 1.2, line.type = "solid", line.alpha = 1, 
+  text.alpha = 1, text.size = 4, text.col = "black",
+  rect.alpha = 0.5, rect.col = NULL, 
+  rect = FALSE, arrow = TRUE, values = TRUE,
+  values.yloc = 0, values.xloc = NULL, or.yloc = 0,
+  arrow.length = NULL, arrow.yloc = NULL, arrow.col = NULL,
+  arrow.xloc.r = NULL, arrow.xloc.l = NULL) 
+{
   
   plot.object <- plot.object + 
     geom_vline(xintercept = or.object$value1, color = line.col, 
@@ -92,33 +93,47 @@ add.oddsratio.into.plot <- function(plot.object, or.object, line.col = "red",
     geom_vline(xintercept = or.object$value2, color = line.col, 
                size = line.size, linetype = line.type, alpha = line.alpha) + 
     annotate("text", x = mean(c(or.object$value2, or.object$value1)),
-             y = min(plot.object$data$se.lwr) + or.height, 
+             y = min(plot.object$data$se.lwr) + or.yloc, 
              label = paste0("OR: \n", round(or.object$oddsratio, 2)),
              color = text.col, size = text.size)
   
+  if (rect) {
+    if (is.null(rect.col)) {
+      rect.col = text.col
+    }
+    # set drawing order to place rect behind smoothing fun
+    plot.object$layers <- c(geom_rect(data = plot.object$data[1,], # avoids multiple rect drawings
+                                      ymin = ggplot_build(plot.object)$layout$panel_ranges[[1]]$y.range[1], 
+                                      ymax = ggplot_build(plot.object)$layout$panel_ranges[[1]]$y.range[2],
+                                      xmin = or.object$value1,
+                                      xmax = or.object$value2, 
+                                      alpha = rect.alpha, fill = rect.col), 
+                            plot.object$layers)
+  }
+
   if (values) {
-    if (is.null(x.shift)) {
+    if (is.null(values.xloc)) {
       # calc x range for x-shift 
-      x.shift <- 0.02
-      x.shift <- (max(plot.object$data$x) - min(plot.object$data$x)) * x.shift
+      values.xloc <- 0.03
+      values.xloc <- (max(plot.object$data$x) - min(plot.object$data$x)) * values.xloc
     }
     
-    if(is.null(arrow.length)) {
+    if (is.null(arrow.length)) {
       # calc arrow length from x axis range
       arrow.length <- (max(plot.object$data$x) - min(plot.object$data$x)) * 0.01
     }
     
-    if(is.null(arrow.height)) {
+    if (is.null(arrow.yloc)) {
       # calc arrow height from y axis range
-      arrow.height <- (max(plot.object$data$y) - min(plot.object$data$y)) * 0.05
+      arrow.yloc <- (max(plot.object$data$y) - min(plot.object$data$y)) * 0.05
     }
     
-    if(is.null(arrow.xloc.l)) {
+    if (is.null(arrow.xloc.l)) {
       # calc arrow shift from x axis range
       arrow.xloc.l <- -(max(plot.object$data$y) - min(plot.object$data$y)) * 0.002
     }
     
-    if(is.null(arrow.xloc.r)) {
+    if (is.null(arrow.xloc.r)) {
       # calc arrow shift from x axis range
       arrow.xloc.r <- (max(plot.object$data$y) - min(plot.object$data$y)) * 0.002
     }
@@ -126,32 +141,39 @@ add.oddsratio.into.plot <- function(plot.object, or.object, line.col = "red",
     
     plot.object <- plot.object + 
 
-      annotate("text", x = or.object$value1 - x.shift,
-               y = min(plot.object$data$se.lwr) + values.height, 
+      annotate("text", x = or.object$value1 - values.xloc,
+               y = min(plot.object$data$se.lwr) + values.yloc, 
                label = or.object$value1,
                color = text.col, alpha = text.alpha, size = text.size) +
-      annotate("text", x = or.object$value2 + x.shift,
-               y = min(plot.object$data$se.lwr) + values.height, 
+      annotate("text", x = or.object$value2 + values.xloc,
+               y = min(plot.object$data$se.lwr) + values.yloc, 
                label = or.object$value2,
                color = text.col, alpha = text.alpha, size = text.size)
     
+    
     if (arrow) {
+      
+      if (is.null(arrow.col)) {
+        # calc arrow shift from x axis range
+        arrow.col <- text.col
+      }
+      
       plot.object <- plot.object + 
-        
+
         # left arrow
-        geom_segment(x = or.object$value1 - x.shift + arrow.xloc.l,
-                     xend = or.object$value1 - x.shift + arrow.length,
-                     y = min(plot.object$data$se.lwr) + values.height + arrow.height,
-                     yend = min(plot.object$data$se.lwr) + values.height + arrow.height,
-                     color = text.col,
-                     arrow = arrow(length = unit(0.2, "cm"))) + 
+        geom_segment(x = or.object$value1 - values.xloc + arrow.xloc.l,
+                     xend = or.object$value1 - values.xloc + arrow.length,
+                     y = min(plot.object$data$se.lwr) + values.yloc + arrow.yloc,
+                     yend = min(plot.object$data$se.lwr) + values.yloc + arrow.yloc,
+                     color = arrow.col,
+                     arrow = arrow(length = unit(0.2, "cm"), type = "closed")) + 
         # right arrow
-        geom_segment(x = or.object$value2 + x.shift + arrow.xloc.r,
-                     xend = or.object$value2 + x.shift - arrow.length,
-                     y = min(plot.object$data$se.lwr) + values.height + arrow.height,
-                     yend = min(plot.object$data$se.lwr) + values.height + arrow.height,
-                     color = text.col, 
-                     arrow = arrow(length = unit(0.2, "cm"))) 
+        geom_segment(x = or.object$value2 + values.xloc + arrow.xloc.r,
+                     xend = or.object$value2 + values.xloc - arrow.length,
+                     y = min(plot.object$data$se.lwr) + values.yloc + arrow.yloc,
+                     yend = min(plot.object$data$se.lwr) + values.yloc + arrow.yloc,
+                     color = arrow.col, 
+                     arrow = arrow(length = unit(0.2, "cm"), type = "closed")) 
     }
     
   }
