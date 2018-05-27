c474ba28cc10ac130e33f268c09216c5f51f4cfc
pat-s <patrick.schratz@gmail.com>
2018-05-27 22:12:42 +0200

    update travis

--- tic.R
+++ tic.R
@@ -1,15 +1,28 @@
-add_package_checks()
+if (Sys.getenv("NB") != "w/ covr" | Sys.getenv("NB") != "w/ lintr") {
+  add_package_checks()
+}
 
 if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+  
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
-
+  
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
+  
+} 
+
+if (Sys.getenv("NB") == "w/ covr" | Sys.getenv("NB") == "w/ lintr") {
+  
+  if (Sys.getenv("NB") == "w/ lintr") {
+    
+    get_stage("after_success") %>% 
+      step_run_code(lintr::lint_package(linters = with_defaults(commented_code_linter = NULL, 
+                                                                closed_curly_linter = closed_curly_linter(allow_single_line = TRUE), 
+                                                                open_curly_linter = open_curly_linter(allow_single_line = TRUE))))
+  } else {
+    get_stage("after_success") %>% 
+      step_run_code(covr::codecov(quiet = FALSE))
+  }
 }
