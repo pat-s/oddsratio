d5792a94482dd3e19045bd597ef48acc5ffd8809
pat-s <patrick.schratz@gmail.com>
2018-08-12 21:07:37 +0200

    remove orphan arg

--- tic.R
+++ tic.R
@@ -3,26 +3,26 @@ if (Sys.getenv("NB") != "w/ covr" | Sys.getenv("NB") != "w/ lintr") {
 }
 
 if (Sys.getenv("id_rsa") != "") {
-  
+
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
-  
+
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
-  
-} 
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+
+}
 
 if (Sys.getenv("NB") == "w/ covr" | Sys.getenv("NB") == "w/ lintr") {
-  
+
   if (Sys.getenv("NB") == "w/ lintr") {
-    
-    get_stage("after_success") %>% 
-      step_run_code(lintr::lint_package(linters = with_defaults(commented_code_linter = NULL, 
-                                                                closed_curly_linter = closed_curly_linter(allow_single_line = TRUE), 
+
+    get_stage("after_success") %>%
+      step_run_code(lintr::lint_package(linters = with_defaults(commented_code_linter = NULL,
+                                                                closed_curly_linter = closed_curly_linter(allow_single_line = TRUE),
                                                                 open_curly_linter = open_curly_linter(allow_single_line = TRUE))))
   } else {
-    get_stage("after_success") %>% 
+    get_stage("after_success") %>%
       step_run_code(covr::codecov(quiet = FALSE))
   }
 }
