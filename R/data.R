3595c70a1d2503edbf59562d14cf93b65334bf79
pat-s <patrick.schratz@gmail.com>
2020-05-23 21:16:02 +0200

    update data_* scripts

--- R/data.R
+++ R/data.R
@@ -10,7 +10,7 @@
 #'
 #' @docType data
 #'
-#' @format a [tibble] randomly created numerical and non-numerical variables
+#' @format A [tibble] of randomly created numerical and non-numerical variables.
 NULL
 
 #' data_glm
@@ -23,8 +23,6 @@ NULL
 #' @keywords datasets
 #' @keywords internal
 #'
-#' @format a [tibble] randomly created numerical and non-numerical variables
+#' @format A [tibble] of randomly created numerical and non-numerical variables.
 #'
-#' @source Taken from \url{http://www.ats.ucla.edu/stat/r/dae/logit.htm}, direct download
-#' link: \url{http://www.ats.ucla.edu/stat/data/binary.csv}
 NULL
